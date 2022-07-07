72992bfba2d7a32e3305b58f1327b2a2c4a8936a
ZloDoc <zlodoc@rambler.ru>
2022-07-07 18:59:47 +0300

    example with random numbers

--- Example003_Sam/Program.cs
+++ Example003_Sam/Program.cs
@@ -1,4 +1,6 @@
-﻿int first=45;
-int second=65;
+﻿int first=new Random().Next(1,100);
+int second=new Random().Next(1,100);
+Console.WriteLine(first);
+Console.WriteLine(second);
 int result= first+second;
-Console.Write(result);
+Console.Write(result); 
