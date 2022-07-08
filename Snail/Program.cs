21c2f83b6141ecbe782df2d731cd4897273d8b6b
ZloDoc <zlodoc@rambler.ru>
2022-07-08 18:05:46 +0300

    Thinking2

--- Snail/Program.cs
+++ Snail/Program.cs
@@ -1,10 +1,21 @@
-﻿int x=0;
-int y=0;
-int [,] snail;
+﻿Console.Clear();
+int n = 5;
+int x = 0;
+int y = 1;
 int nums = 1;
-for (nums==25;nums++)
+//int[,] snail;
+for (; x <= n-1; x++)
 {
 
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine(nums);
+    nums = nums + 1;
+}
+//Console.WriteLine(x);
+for (; y <= n - 1; y++)
+{
 
-
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine(nums);
+    nums = nums + 1;
 }
